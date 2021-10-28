1e0917454bc985cc2db55d61c10ec6ad0f07cf3c
jindal2209 <shubham7811@yahoo.com>
2021-10-28 11:48:01 +0530

    project added

--- src/App.js
+++ src/App.js
@@ -22,35 +22,6 @@ function changeScrollWidth(event) {
 	document.getElementById("scrollPos").style.width = scrolled + "%";
 }
 
-function PrecentageBar(props) {
-	var wid = props.val
-	return (
-		<div className="row percentageBar">
-			<div className='col-md-12'>
-				<div className='row'>
-					<div className='col-md-12'>
-						<div className='float-start fw-bold'>
-							<img src={props.src} alt={props.name} width='30' height='30' style={{ padding: '5px' }} />
-							{props.name}
-						</div>
-						<div className='float-end fw-bold'>{wid}%</div>
-					</div>
-				</div>
-				<div className='row'>
-					<div className="progress" style={{ padding: 0 }}>
-						<div
-							className="progress-bar bg-info" role="progressbar"
-							style={{ width: `${wid}%`, transition: '0ms ease-out' }}
-							aria-valuenow={props.val}
-							aria-valuemin="0"
-							aria-valuemax="100"></div>
-					</div>
-				</div>
-			</div>
-		</div>
-	)
-}
-
 function Navbar() {
 	return (
 		<nav>
@@ -150,6 +121,109 @@ function About() {
 	)
 }
 
+function PrecentagelBar(props) {
+	return (
+		<div className='text-center m-1'>
+			<img className='m-3' src={props.src} alt={props.name} width='50' height='50' style={{ padding: '5px' }} />
+
+			{props.name}
+		</div>
+	)
+}
+
+function PrecentageBar(props) {
+	var wid = props.val
+	return (
+		<div className="row percentageBar">
+			<div className='col-md-12'>
+				<div className='row'>
+					<div className='col-md-12'>
+						<div className='float-start fw-bold'>
+							<img src={props.src} alt={props.name} width='30' height='30' style={{ padding: '5px' }} />
+							{props.name}
+						</div>
+						<div className='float-end fw-bold'>{wid}%</div>
+					</div>
+				</div>
+				<div className='row'>
+					<div className="progress" style={{ padding: 0 }}>
+						<div
+							className="progress-bar bg-info" role="progressbar"
+							style={{ width: `${wid}%`, transition: '0ms ease-out' }}
+							aria-valuenow={props.val}
+							aria-valuemin="0"
+							aria-valuemax="100"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+function Skislls() {
+	return (
+		<div className='row my-5 pb-5' id='skills'>
+			<h1 className='header'>
+				Skills
+			</h1>
+			<div className='d-flex flex-wrap flex-row justify-content-start align-items-start' data-aos='zoom-in'>
+				<PrecentageBar
+					name='C++'
+					val='80'
+					src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-line.svg"
+				/>
+				<PrecentageBar
+					name="Python"
+					val='80'
+					src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg"
+				/>
+				<PrecentageBar
+					name="Javascript"
+					val='80'
+					src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg"
+				/>
+				<PrecentageBar
+					name='SQL'
+					val='60'
+					src="https://img.icons8.com/plasticine/100/000000/oracle-pl-sql--v3.png"
+				/>
+				<PrecentageBar
+					name='AWS'
+					val='70'
+					src="https://raw.githubusercontent.com/devicons/devicon/master/icons/amazonwebservices/amazonwebservices-original.svg"
+				/>
+				<PrecentageBar
+					name='Django'
+					val='85'
+					src="https://raw.githubusercontent.com/devicons/devicon/9f4f5cdb393299a81125eb5127929ea7bfe42889/icons/django/django-original.svg"
+				/>
+				<PrecentageBar
+					name='React'
+					val='70'
+					src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg"
+				/>
+				<PrecentageBar
+					name='CSS'
+					val='60'
+					src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-plain.svg"
+				/>
+				<PrecentageBar
+					name='HTML'
+					val='80'
+					src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-plain.svg"
+				/>
+				<PrecentageBar
+					name='Bootstrap'
+					val='80'
+					src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain.svg"
+				/>
+			</div>
+		</div>
+
+	)
+}
+
+
 function Skills() {
 	return (
 		<div className='row my-5' id='skills'>
@@ -236,6 +310,13 @@ function Projects() {
 				Projects
 			</h1>
 			<div className=''>
+				<ProjectComponent
+					name='URL Shortener'
+					desc='A URL shortener using hashing.'
+					link='https://bitny.herokuapp.com/'
+					hosted={true}
+					available={true}
+				/>
 				<ProjectComponent
 					name='Algolizer'
 					desc='An application showing visulization of various algorithms like sorting, backtracking, graphs, heaps tries.'
@@ -346,7 +427,7 @@ function Contact() {
 				<h3>
 					Lets talk about work
 				</h3>
-				<i className="fas fa-globe-asia"></i>Location: New Delhi,India
+				<i className="fas fa-globe-asia"></i>Address: New Delhi,India
 				<br />
 				<i className="fas fa-envelope"></i>Email: shubham7811@yahoo.com
 			</div>
@@ -371,7 +452,8 @@ function Contact() {
 function Footer() {
 	return (
 		<footer className="footer">
-			Shubham Jindal, &#169; Copyright 2021
+			{/* Shubham Jindal, &#169; Copyright 2021 */}
+			<h6><b>Shubham Jindal</b></h6>
 		</footer>
 	)
 }
