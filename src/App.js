aaf838c241fd69668bdbaf9edde4c6943101fc48
jindal2209 <shubham7811@yahoo.com>
2022-03-06 22:22:56 +0530

    ''

--- src/App.js
+++ src/App.js
@@ -401,9 +401,10 @@ function Experience() {
 				<ExperienceComponent
 					company_name='Shiprocket'
 					position='Software Enginner Intern'
-					duration='January 2022 - Present'
+					duration='January 2022 - Februrary 2022'
 					desc={[
-						'Working with fullfilment team in front-end development using Angular.'
+						'mplemented custom calendar for scheduling slots which will remove the unnecessary crowd at the warehouses by 90% and help control the number of employees at warehouses.',
+						'Worked on automated integration of customer’s shopify stores with shiprocket application which will remove the manual installation process by 70% and reduce workload on sales team by 50%'
 					]}
 				/>
 				<ExperienceComponent
@@ -412,8 +413,8 @@ function Experience() {
 					duration='August 2021 - October 2021'
 					desc={[
 						'Worked with team on design and development of Customer relationship management Teleduce built using Django.',
-						'Implemented some UI based feature to make application more interactive.',
-						'Developed a customer portal application from which customer can raise tickets and connect with the agent to resolve issues.',
+						'mplemented UI based feature to make application more interactive which increased customer base by 10%.',
+						'Developed a customer portal application from which customer can raise tickets and connect with the agent to resolve issues which reduced the manual work by 20%',
 						'Got to learn about Django, cloud infrastructure, CI/CD pipelines, and lead management in CRMs.'
 					]}
 				/>
