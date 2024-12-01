29d1536601e57b2f76b837ef2f73ff1d88c94354
jindal2209 <shubham7811@yahoo.com>
2024-12-01 20:41:15 +0530

    update date

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -12,15 +12,25 @@ export default function About() {
               Hello,<br /> My name is Shubham Jindal.
             </span>
             <br />
-            <div className='h3 pb-5'>
-              I am a Web Developer and a competitive programmer.
-              I code in C++, Python and JavaScript. I also ❤️ hacking.
+            <div className='h4 pb-5 pt-1'>
+              I am a software engineer with primary focus on backend.
+              I love solving real life problems and technical problems with experience in optimizing systems and enhacing product and user experience.
+              During my free time I like to explore nature, solve sudoku and rubik's cube.
+              {/* I code in multiple languaC++, Python and JavaScript. I also ❤️ hacking. */}
             </div>
           </div>
         </div>
 
         <div className='row'>
           <div className='col-md-12'>
+            <a
+              className={`introLinks introLinks-hr`}
+              href="https://www.topmate.io/jindal_shubham"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={process.env.PUBLIC_URL + "/topmate.png"} width="50em" height="50em" />
+            </a>
             {profilesData.map((val, key) => (
               <a
                 key={key}
